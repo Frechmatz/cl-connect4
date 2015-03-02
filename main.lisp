9045564415abae8cc0ef5f8adca60e00aa247464
Oliver Völtz <olli@Olivers-MBP.Speedport_W723_V_Typ_A_1_01_009>
2015-03-02 22:32:20 +0100

    Create empty array

---
+++ main.lisp
@@ -0,0 +1,22 @@
+
+#|
+
+ #####                                           #       
+#     #  ####  #    # #    # ######  ####  ##### #    #  
+#       #    # ##   # ##   # #      #    #   #   #    #  
+#       #    # # #  # # #  # #####  #        #   #    #  
+#       #    # #  # # #  # # #      #        #   ####### 
+#     # #    # #   ## #   ## #      #    #   #        #  
+ #####   ####  #    # #    # ######  ####    #        #  
+                                                         
+|#
+
+
+(defparameter *WIDTH* 7)
+(defparameter *HEIGHT* 6)
+
+
+; Quasiquoting
+(defun field-create () (make-array `(,*HEIGHT* ,*WIDTH*)))
+
+(print (field-create))
