d679f124732c4d238fe756de67ed19ec7dc81917
Oliver Völtz <olli@Olivers-MBP.Speedport_W723_V_Typ_A_1_01_009>
2015-03-02 23:26:57 +0100

    Added border

--- main.lisp
+++ main.lisp
@@ -17,6 +17,14 @@
 
 
 ; Quasiquoting
-(defun field-create () (make-array `(,*HEIGHT* ,*WIDTH*)))
+(defun field-create () (make-array `( ,(+ 2 *HEIGHT*) ,(+ 2 *WIDTH*)) :initial-element 0))
+
+(defun field-init-border  (f)
+  (dotimes (x (+ 2 *WIDTH*)) (setf (aref f 0 x) 'X))
+  (dotimes (x (+ 2 *WIDTH*)) (setf (aref f (+ 1 *HEIGHT*) x) 'X))
+  (dotimes (y (+ 2 *HEIGHT*)) (setf (aref f y 0) 'X))
+  (dotimes (y (+ 2 *HEIGHT*)) (setf (aref f y (+ 1 *WIDTH*)) 'X))
+)
 
 (print (field-create))
+
