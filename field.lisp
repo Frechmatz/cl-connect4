dcb430648e4307ce5cf6009825cdc72e055cc8be
Oliver Voeltz <olli@frechmatz.com>
2015-07-06 22:22:27 +0200

    New implementation of board-formatter

--- field.lisp
+++ field.lisp
@@ -195,19 +195,53 @@ Common Connect4 board related functionality
   ((highlight-cells :initarg highlight-cells :initform '())
    ))
 
+(defgeneric format-horizontal-cell-margin(formatter)
+  (:documentation "Format the horizontal space between two cells in a row"))
+
 (defgeneric format-cell-value (formatter cell-value)
   (:documentation "Formats a single cell represented by its value"))
 
 (defgeneric format-cell (formatter board x y)
   (:documentation "Formats a single cell represented by its position within the board"))
 
+(defgeneric format-border-top (formatter x)
+  (:documentation "Format the header of given column"))
+
+(defgeneric format-border-bottom (formatter x)
+  (:documentation "Format the footer of given column"))
+
+(defgeneric format-border-left (formatter y)
+  (:documentation "Format the left border of given row"))
+
+(defgeneric format-border-right (formatter y)
+  (:documentation "Format the right border of given row"))
+
 ;; Default impl
 (defmethod format-cell-value ( (formatter cell-formatter) cell-value)
-  cell-value
-  )
+  (cond
+   ((equal cell-value *WHITE*) "W")
+   ((equal cell-value *BLACK*) "B")
+   (t "_")
+   ))
+
+(defmethod format-horizontal-cell-margin( (formatter cell-formatter))
+  " ")
 
 (defmethod format-cell ( (formatter cell-formatter) board x y)
   (format-cell-value formatter (get-field board x y))
+   )
+
+(defmethod format-border-top ( (formatter cell-formatter) x)
+  (format nil "~a" x)
+   )
+(defmethod format-border-bottom ( (formatter cell-formatter) x)
+  (format nil "~a" x)
+   )
+(defmethod format-border-left ( (formatter cell-formatter) y)
+  (format nil "~a" y)
+   )
+(defmethod format-border-right ( (formatter cell-formatter) y)
+  (format nil "~a" y)
    )
 
 (defclass colorful-cell-formatter (cell-formatter)
@@ -219,39 +253,43 @@ Common Connect4 board related functionality
   (cond
    ((equal cell-value *WHITE*) (format nil "~c[32mW~c[0m" #\Esc #\Esc))
    ((equal cell-value *BLACK*) (format nil "~c[31mB~c[0m" #\Esc #\Esc))
-   (t cell-value)
+   (t "_")
    ))
 
 ;;
 ;; Pretty print a board using a formatter
-;; This code is crap :(
-;; Todo: re-think the formatting
 ;;
 (defun format-board (board &optional cell-formatter )
   (if (not cell-formatter) (setf cell-formatter (make-instance 'cell-formatter)))
-  (let ((rows '()) (footer '()) (header '() ))
-    (push *BORDER* header)
-    (push *BORDER* footer)
-    (dotimes (x (get-board-width board))
-      (push x header)
-      (push x footer))
-    (push *BORDER* header)
-    (push *BORDER* footer)
-    (push (nreverse header) rows)
-    (dotimes (y (get-board-height board))
-      (let ((row '()))
-	(push *BORDER* row)
-	(dotimes (x (get-board-width board))
-	  (push (format-cell cell-formatter board x y) row)
+  (let ((inner-height (get-board-height board)) (inner-width (get-board-width board)))
+    (let ( (formatted-board (make-array (list (+ 2 inner-height) (+ 2 inner-width)) :initial-element "X")))
+      ;; format inner field
+      (dotimes (y inner-height)
+	(dotimes (x inner-width)
+	  (setf (aref formatted-board (+ y 1) (+ x 1)) (format-cell cell-formatter board x y))
+	  ))
+      ;; format top/bottom border
+      (dotimes (x inner-width)
+	  (setf (aref formatted-board 0 (+ x 1)) (format-border-top cell-formatter x))
+	  (setf (aref formatted-board (+ inner-height 1) (+ x 1)) (format-border-bottom cell-formatter x))
 	  )
-	(push *BORDER* row)
-	(push (nreverse row) rows)
-	))
-    (push (nreverse footer) rows)
-    (dolist (l (nreverse rows))
-      (princ l)
-      (princ #\newline)
-      )
-    nil))
-
+      ;; format left/right border
+      (dotimes (y inner-height)
+	  (setf (aref formatted-board (+ y 1) 0) (format-border-left cell-formatter y))
+	  (setf (aref formatted-board (+ y 1) (+ inner-width 1)) (format-border-right cell-formatter y))
+	  )
+      ;; inner function that joins all strings of a given row
+      (flet ((join-row (y)
+		       (let ((result (aref formatted-board y 0)))
+			 (dotimes (x (+ (array-dimension formatted-board 1) -1))
+			   (setf result (concatenate 'string result (format-horizontal-cell-margin cell-formatter)))
+			   (setf result (concatenate 'string result (aref formatted-board y (+ x 1))))
+			   )
+			 result)))
+	    (dotimes (y (array-dimension formatted-board 0))
+	      (princ (join-row y))
+	      (princ #\newline))
+	    )))
+  nil
+  )
 
