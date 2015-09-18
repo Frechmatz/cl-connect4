3183923541270707e8ed3f5b38d0b1d4e348cc2c
Oliver Voeltz <frechmatz@gmx.de>
2015-09-18 04:23:05 +0200

    Removed debug output

--- test/column-weights.lisp
+++ test/column-weights.lisp
@@ -16,7 +16,7 @@
 
 (define-test test-column-weight-2 ()
 	     (let ( (weights (connect4::calc-column-weights 6 t)))
-	       (format t "test-column-weight-2: Calculated column weights: ~a~%" weights)
+	       ;;(format t "test-column-weight-2: Calculated column weights: ~a~%" weights)
 	       (assert-equal (aref weights 3) 1.0 (format t "test-column-weight-2: Center column does not have weight of 1.0"))
 	       (assert-true (< (aref weights 2) (aref weights 3)) (format t "test-column-weight-2: Weight left of center is not smaller than weight of center"))
 	       (assert-true (< (aref weights 4) (aref weights 3)) (format t "test-column-weight-2: Weight right of center is not smaller than weight of center"))
