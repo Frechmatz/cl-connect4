4f4e5aa2e08f17634415ad43c203f64261eb2fe9
Oliver Voeltz <frechmatz@gmx.de>
2016-02-17 23:17:21 +0100

    Styling

--- src/web-server/css.lisp
+++ src/web-server/css.lisp
@@ -5,10 +5,11 @@
   "linen"
   )
 
-(defun mausi-style ()
-  (cl-css:css '(
-		(.mausi :background-color "mausi"))))
-
+(defun display-flex (selector)
+  (cl-css:css `((,selector
+    :display "flex"
+    :display "-webkit-flex"
+))))
 
 (defun css ()
   (cl-css:css `(
@@ -16,7 +17,7 @@
 		(.page-wrapper :padding "20px")
 		
 		;; Placement of main blocks
-		(.header :height "20%")
+		(.header :height "20vh")
 		(.body :margin-top "20px")
 		(.footer :margin-top "20px")
 
@@ -25,12 +26,36 @@
 		;; Body: Consists of Playground and CCFI Console
 		;;
 		
+		;; box layout of body
+		;; ;; Playground (board)
+		;; (".body .board" :width "40%" :float "left")
+		;; ;; CCFI console
+		;; (".body .console" :width "30%" :float "right"
+		;; 		  :height "200px"
+		;; 		  )
+
+		;; flex layout of body
 		;; Playground (board)
-		(".body .board" :width "40%" :float "left")
+		(.body
+		 :flex-flow "row"
+		 :height "60vh" ;; Höhe des Bodies = 60% 
+		 )
+		(,(display-flex ".body"))
+
+		
+		(".body .board"
+		 :width "100%" ;; Breite des Feldes = Höhe des Parents
+		 ;; :flex "1"
+		 )
 		;; CCFI console
-		(".body .console" :width "30%" :float "right"
-				  :height "200px"
-				  )
+		(".body .console"
+		 ;;:width "100px"
+		 :background-color "yellow"
+		 :flex 1
+		 :-webkit-flex 1
+		 ;;:display "flex"
+		 )
+
 		(".footer" :clear "both" :color "yellow") 
 		
 		;;
@@ -41,12 +66,21 @@
 		;;
 		;; Inner CCFI console
 		;;
-		(".console .console-content" :width "100%":height "100%")
-		(".console .console-textarea" :width "100%" :height "100%")
+		(".console .console-content"
+		 :width "100%"
+		 :height "100%"
+		 ;;:display "flex"
+		 )
+		(".console .console-textarea"
+		 :width "100%"
+		 :height "100%"
+		 ;;:flex "1"
+		 )
 		
-		;;
-		;; Colors, fonts, background images, etc
-		;;
+		;; Styling: Body
+		(body :background-color ,(body-background-color))
+
+		;; Styling: Header
 		(.header
 		 :background-color "blue"
 		 ;; :background-image "url(/static/made-with-lisp-logo.png)"
@@ -56,9 +90,6 @@
 		 :background-position "right"
 		 :position relative
 		 )
-		(.footer
-		 :background-color "blue")
-		(body :background-color ,(body-background-color))
 		(".header h1" 
 		 :margin "0"
 		 :color "yellow"
@@ -67,13 +98,18 @@
 		 :-webkit-transform "translate(0, -50%)" ;;; Safari
 		 :transform "translate(0, -50%)"
 		 )
+
+		;; Styling: Footer
+		(.footer
+		 :background-color "blue")
+
+		;; Styling: Console
 		(.console-textarea
 		 :background-color "black"
 		 :color "lime"
 		 )
 
-		;; Board styling
-		;; https://www.w3.org/wiki/Styling_tables
+		;; Styling: Board
 		(.board-table
 		 :border "1px solid black;"
 		 :border-collapse "collapse"
@@ -88,5 +124,4 @@
 		 :background-color "white")
 		(,(format nil ".board-cell[token=~ao~a]" #\" #\" )
 		 :background-color "green")
-		(,(mausi-style))
 	      )))
